69987e7f89feaac42b023eb062eee0e721e59c15
ErikaLeona <107769103+ErikaLeona@users.noreply.github.com>
2026-06-29 15:13:58 +0300

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment ={
   weatherApiBaseUrl: 'http://api.weatherapi.com/v1/forecast.json',
   apiKeyName:'key',
-  apiKeyValue:'e9468ac9f5d349f684e124328262506',
+  apiKeyValue:'YOUR API KEY', //Change here
 };
